58ad565c569889f9f393938ae111650937fcd732
jessanDaniel <jessandaniel@gmail.com>
2022-09-27 16:15:57 +0530

    dfsa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
+  
+  constructor(private http:HttpClient){}
+
+  url='http://localhost:5000/train';
+
+  dates=this.http.get(this.url).subscribe((resp:any)=>{
+    let resp_dates=JSON.parse(resp.dates)
+    let resp_actual=JSON.parse(resp.actual_sales)
+    let resp_predicted=JSON.parse(resp.predicted_sales)
+    console.log(resp_dates[0],resp_actual[0],resp_predicted[0])
+  })
 }
